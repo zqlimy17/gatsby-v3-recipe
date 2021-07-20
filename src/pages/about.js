90ee5db3f43eca3cc081fe2d746af7d4742cb29a
zqlimy17 <zqlimy@gmail.com>
2021-07-20 17:17:02 +0800

    set up pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
+const About = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>About Page</h1>
     </Layout>
   )
 }
+
+export default About
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
+const Recipe = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Recipe Page</h1>
     </Layout>
   )
 }
+
+export default Recipe
