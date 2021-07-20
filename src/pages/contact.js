7f79dab6a26f6f56aaa3e69cd20846dd23567367
zqlimy17 <zqlimy@gmail.com>
2021-07-20 17:55:52 +0800

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,45 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to get in touch?</h3>
+            <p>
+              Bacon ipsum dolor amet kielbasa buffalo ribeye, fatback ham
+              turducken.
+            </p>
+            <p>
+              Meatball fatback cow, frankfurter leberkas beef bresaola rump
+              meatloaf tenderloin.
+            </p>
+            <p>
+              Pastrami jerky burgdoggen tongue, meatball short loin chislic
+              t-bone venison. Pork kevin prosciutto beef ribs, landjaeger tail
+              tongue jerky ground round t-bone.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">your message</label>
+                <textarea type="text" name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
