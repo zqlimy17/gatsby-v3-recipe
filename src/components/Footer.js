42cc82135d25191e7c1090a46dc576b55de53ad2
zqlimy17 <zqlimy@gmail.com>
2021-07-20 17:42:50 +0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is the footer</h2>
+    <div className="page-footer">
+      <p>
+        &copy; 2021 <span>Simply Recipes</span>. Built with{" "}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a> by{" "}
+        <a href="https://zqlimy.com/">zqlimy</a>.
+      </p>
     </div>
   )
 }
