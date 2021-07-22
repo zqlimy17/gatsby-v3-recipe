d010edf588d053fa0efdb87cbf13015f55db2f2c
zqlimy17 <zqlimy@gmail.com>
2021-07-22 13:24:48 +0800

    about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,36 @@
 import React from "react"
 import Layout from "../components/Layout"
-
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>About SimplyRecipes</h2>
+            <p>
+              Spicy jalapeno bacon ipsum dolor amet venison beef ribs shank ball
+              tip filet mignon porchetta, beef shankle brisket pancetta picanha
+              turducken buffalo short loin andouille.
+            </p>
+            <p>
+              T-bone cupim turkey sirloin bresaola strip steak kielbasa kevin
+              capicola shankle. Beef bacon pancetta, jowl pork belly andouille
+              meatball short ribs kevin salami sirloin.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt in bowl"
+            className="about-img"
+            placeholder="tracedSVG"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
