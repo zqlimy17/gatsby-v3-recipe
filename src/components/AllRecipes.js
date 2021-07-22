a69519ea04ec23326e3304a4eb9ee7b279a8a731
zqlimy17 <zqlimy@gmail.com>
2021-07-22 23:06:06 +0800

    added recipes to about us page

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,13 +1,37 @@
 import React from "react";
 import TagList from "./TagList";
 import RecipesList from "./RecipesList";
+import { graphql, useStaticQuery } from "gatsby";
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`;
+
 const AllRecipes = () => {
+  const data = useStaticQuery(query);
+  const recipes = data.allContentfulRecipe.nodes;
+  console.log(recipes);
   return (
-    <div>
+    <section className="recipes-container">
       all recipes
-      <TagList />
-      <RecipesList />
-    </div>
+      <TagList recipes={recipes} />
+      <RecipesList recipes={recipes} />
+    </section>
   );
 };
 
