d3ef8b82e5f6e9a095256dec157064ab4a70556d
zqlimy17 <zqlimy@gmail.com>
2021-07-24 18:26:58 +0800

    imports for recipeTemplate

--- src/pages/recipes/{ContentfulRecipe.title}.js
+++ src/pages/recipes/{ContentfulRecipe.title}.js
@@ -1,15 +1,51 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsClockHistory, BsClock, BsPeople } from "react-icons";
 
-const RecipeTemplate = props => {
-  console.log(props);
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe;
+  const pathToImage = getImage(image);
+  const { tags, instructions, ingredients, tools } = content;
   return (
     <Layout>
       <main className="page">
-        <h2></h2>
+        <h2>{title}</h2>
       </main>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      prepTime
+      description {
+        description
+      }
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+    }
+  }
+`;
 
 export default RecipeTemplate;
